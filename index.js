070cabd2ac2b53c8fc503f2fca88598833de07bd
keith-mikel <keith.mikel@e5sol.com>
2023-07-12 13:42:48 -0500

    svg file generating, need to make specifications for each shape in svg.js still

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-//import questions
 const logoInput = require('./lib/inq');
-const svgInput = require('./lib/svg')
+const svgInput = require('./lib/svg');
 
-logoInput.logoPropmt();
+logoInput.logoPropmt()
+  .then((answers) => {
+    const { letters, textColor, shape, shapeColor } = answers;
+    svgInput.generateSVG(letters, textColor, shape, shapeColor);
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
+
 
