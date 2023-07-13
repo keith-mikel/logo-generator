0d80c6149ad2999df4c8c2c3ae932dd0929f5540
keith-mikel <keith.mikel@e5sol.com>
2023-07-13 12:15:28 -0500

    added checks and finshed logo generation

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const svgInput = require('./lib/svg');
 
 logoInput.logoPropmt()
   .then((answers) => {
-    const { letters, textColor, shape, shapeColor } = answers;
-    svgInput.generateSVG(letters, textColor, shape, shapeColor);
+    const {id, text, textColor, shape, shapeColor } = answers;
+    svgInput.generateSVG(id, text, textColor, shape, shapeColor);
   })
   .catch((error) => {
     console.error('An error occurred:', error);
